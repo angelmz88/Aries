e5265323ce86ed175dee52c3aa9056de256ce784
angelmz88 <joseangelmarquez080802@gmail.com>
2024-04-28 15:18:22 -0600

    Front end parte 1 V1

---
+++ JS/registro-nota.js
@@ -0,0 +1,12 @@
+document
+  .getElementById("form-register")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevenir el envío del formulario por defecto
+
+    // Obtener los valores de los campos del formulario
+
+    // Validar los valores (puedes agregar tus propias validaciones aquí)
+
+    // Mostrar un mensaje de alerta con los valores del formulario
+    alert("¡Formulario enviado!");
+  });
