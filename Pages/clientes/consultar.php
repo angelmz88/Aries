fa81f12bd2a3ccdecb2456e63f6cba1535eefabc
angelmz88 <joseangelmarquez080802@gmail.com>
2024-05-17 00:19:57 -0600

    Visualizaciones básicas terminadas

--- Pages/clientes/consultar.php
+++ Pages/clientes/consultar.php
@@ -1,7 +1,44 @@
 <?php
 include ("clientes/../../../php/deep_sesion.php");
-?>
+include ("clientes/../../../php/bd.php");
 
+// Función para obtener los datos de la base de datos
+function obtenerClientes($term = '')
+{
+  global $conn;
+  $sql = "SELECT * FROM clientes";
+  if ($term != '') {
+    $sql .= " WHERE Numero_Telefono_PK LIKE '%$term%' OR Primer_Nombre LIKE '%$term%' OR Segundo_Nombre LIKE '%$term%' 
+    OR Primer_Apellido LIKE '%$term%' OR Segundo_Apellido LIKE '%$term%' OR Calle LIKE '%$term%' OR Numero_Exterior LIKE '%$term%'";
+  }
+  $result = $conn->query($sql);
+
+  $clientes = array();
+
+  if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+      $clientes[] = $row;
+    }
+  }
+  return $clientes;
+}
+
+// Inicializar la variable $clientes para evitar errores
+$clientes = array();
+
+// Si es una solicitud AJAX, devolver los resultados en JSON
+if (isset($_GET['term'])) {
+  header('Content-Type: application/json'); // Asegura que el contenido sea JSON
+  $term = $_GET['term'];
+  $clientes = obtenerClientes($term);
+  echo json_encode($clientes);
+  $conn->close();
+  exit();
+}
+
+// Obtener todos los clientes al cargar la página
+$clientes = obtenerClientes();
+?>
 <!DOCTYPE html>
 <html lang="es">
 
@@ -13,6 +50,38 @@ include ("clientes/../../../php/deep_sesion.php");
   <link rel="stylesheet" href="../../css/consulta.css" />
   <link rel="shortcut icon" href="../../img/global/lavanderia.png" />
   <title>Clientes</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script>
+    $(document).ready(function () {
+      $('#search').on('keyup', function () {
+        var searchTerm = $(this).val();
+        $.ajax({
+          url: '',
+          type: 'GET',
+          data: { term: searchTerm },
+          dataType: 'json',
+          success: function (data) {
+            var tableContent = '<table><tr><th>Telefono</th><th>Primer Nombre</th><th>Segundo Nombre</th><th>Primer Apellido</th><th>Segundo Apellido</th><th>Calle</th><th>Numero Exterior</th></tr>';
+            if (data.length > 0) {
+              $.each(data, function (index, cliente) {
+                tableContent += '<tr><td>' + cliente.Numero_Telefono_PK + '</td><td>' + cliente.Primer_Nombre +
+                  '</td><td>' + cliente.Segundo_Nombre + '</td><td>' + cliente.Primer_Apellido +
+                  '</td><td>' + cliente.Segundo_Apellido + '</td><td>' + cliente.Calle +
+                  '</td><td>' + cliente.Numero_Exterior + '</td><td><a href="actualizar.php?Numero_Telefono_PK=' + cliente.Numero_Telefono_PK + '">Editar</a></td></tr>';
+              });
+            } else {
+              tableContent += '<tr><td colspan="8">No se encontraron resultados</td></tr>';
+            }
+            tableContent += '</table>';
+            $('#resultados').html(tableContent);
+          },
+          error: function (jqXHR, textStatus, errorThrown) {
+            console.log(textStatus, errorThrown);
+          }
+        });
+      });
+    });
+  </script>
 </head>
 
 <body>
@@ -26,8 +95,40 @@ include ("clientes/../../../php/deep_sesion.php");
   </section>
   <section class="options">
     <div class="buscador">
-      <input type="text" />
+      <input type="text" id="search" placeholder="Buscar cliente..." />
     </div>
+  </section>
+  <section id="resultados" class="resultados">
+    <table class="table">
+      <tr>
+        <th>Telefono</th>
+        <th>Primer Nombre</th>
+        <th>Segundo Nombre</th>
+        <th>Primer Apellido</th>
+        <th>Segundo Apellido</th>
+        <th>Calle</th>
+        <th>Numero Exterior</th>
+        <th>Acciones</th>
+      </tr>
+      <?php if (count($clientes) > 0): ?>
+        <?php foreach ($clientes as $cliente): ?>
+          <tr>
+            <td><?php echo $cliente["Numero_Telefono_PK"]; ?></td>
+            <td><?php echo $cliente["Primer_Nombre"]; ?></td>
+            <td><?php echo $cliente["Segundo_Nombre"]; ?></td>
+            <td><?php echo $cliente["Primer_Apellido"]; ?></td>
+            <td><?php echo $cliente["Segundo_Apellido"]; ?></td>
+            <td><?php echo $cliente["Calle"]; ?></td>
+            <td><?php echo $cliente["Numero_Exterior"]; ?></td>
+            <td><a href="actualizar.php?Numero_Telefono_PK=<?php echo $cliente['Numero_Telefono_PK']; ?>">Editar</a></td>
+          </tr>
+        <?php endforeach; ?>
+      <?php else: ?>
+        <tr>
+          <td colspan="8">No se encontraron resultados</td>
+        </tr>
+      <?php endif; ?>
+    </table>
   </section>
   <footer class="footer">
     <a href="../cliente.php" class="btn_salir">Regresar</a>
--- Pages/notas/buscar.php
+++ Pages/notas/buscar.php
@@ -1,7 +1,44 @@
 <?php
 include ("notas/../../../php/deep_sesion.php");
-?>
+include ("notas/../../../php/bd.php");
 
+// Función para obtener los datos de la base de datos
+function obtenerNotas($term = '')
+{
+  global $conn;
+  $sql = "SELECT * FROM notas";
+  if ($term != '') {
+    $sql .= " WHERE Folio_Nota_PK LIKE '%$term%' OR Numero_Telefono_Cliente_FK LIKE '%$term%' OR Numero_Telefono_Empleado_FK LIKE '%$term%' 
+    OR Tipo_Servicio LIKE '%$term%' OR Fecha_Entrega_Estimada LIKE '%$term%' OR Hora_Entrega_Estimada LIKE '%$term%'";
+  }
+  $result = $conn->query($sql);
+
+  $notas = array();
+
+  if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+      $notas[] = $row;
+    }
+  }
+  return $notas;
+}
+
+// Inicializar la variable $notas para evitar errores
+$notas = array();
+
+// Si es una solicitud AJAX, devolver los resultados en JSON
+if (isset($_GET['term'])) {
+  header('Content-Type: application/json'); // Asegura que el contenido sea JSON
+  $term = $_GET['term'];
+  $notas = obtenerNotas($term);
+  echo json_encode($notas);
+  $conn->close();
+  exit();
+}
+
+// Obtener todas las notas al cargar la página
+$notas = obtenerNotas();
+?>
 <!DOCTYPE html>
 <html lang="es">
 
@@ -13,6 +50,38 @@ include ("notas/../../../php/deep_sesion.php");
   <link rel="stylesheet" href="../../css/consulta.css" />
   <link rel="shortcut icon" href="../../img/global/lavanderia.png" />
   <title>Notas</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script>
+    $(document).ready(function () {
+      $('#search').on('keyup', function () {
+        var searchTerm = $(this).val();
+        $.ajax({
+          url: '',
+          type: 'GET',
+          data: { term: searchTerm },
+          dataType: 'json',
+          success: function (data) {
+            var tableContent = '<table><tr><th>Folio Nota</th><th>Telefono Cliente</th><th>Telefono Empleado</th><th>Tipo Servicio</th><th>Fecha Entrega Estimada</th><th>Hora Entrega Estimada</th><th>Acciones</th></tr>';
+            if (data.length > 0) {
+              $.each(data, function (index, nota) {
+                tableContent += '<tr><td>' + nota.Folio_Nota_PK + '</td><td>' + nota.Numero_Telefono_Cliente_FK +
+                  '</td><td>' + nota.Numero_Telefono_Empleado_FK + '</td><td>' + nota.Tipo_Servicio +
+                  '</td><td>' + nota.Fecha_Entrega_Estimada + '</td><td>' + nota.Hora_Entrega_Estimada +
+                  '</td><td><a href="actualizar.php?Folio_Nota_PK=' + nota.Folio_Nota_PK + '">Editar</a></td></tr>';
+              });
+            } else {
+              tableContent += '<tr><td colspan="7">No se encontraron resultados</td></tr>';
+            }
+            tableContent += '</table>';
+            $('#resultados').html(tableContent);
+          },
+          error: function (jqXHR, textStatus, errorThrown) {
+            console.log(textStatus, errorThrown);
+          }
+        });
+      });
+    });
+  </script>
 </head>
 
 <body>
@@ -21,13 +90,43 @@ include ("notas/../../../php/deep_sesion.php");
   </header>
   <section class="hero">
     <div class="hero-cover">
-      <h1>CONSULTAR NOTA</h1>
+      <h1>CONSULTAR NOTAS</h1>
     </div>
   </section>
   <section class="options">
     <div class="buscador">
-      <input type="text" />
+      <input type="text" id="search" placeholder="Buscar nota..." />
     </div>
+  </section>
+  <section id="resultados" class="resultados">
+    <table class="table">
+      <tr>
+        <th>Folio Nota</th>
+        <th>Telefono Cliente</th>
+        <th>Telefono Empleado</th>
+        <th>Tipo Servicio</th>
+        <th>Fecha Entrega Estimada</th>
+        <th>Hora Entrega Estimada</th>
+        <th>Acciones</th>
+      </tr>
+      <?php if (count($notas) > 0): ?>
+        <?php foreach ($notas as $nota): ?>
+          <tr>
+            <td><?php echo $nota["Folio_Nota_PK"]; ?></td>
+            <td><?php echo $nota["Numero_Telefono_Cliente_FK"]; ?></td>
+            <td><?php echo $nota["Numero_Telefono_Empleado_FK"]; ?></td>
+            <td><?php echo $nota["Tipo_Servicio"]; ?></td>
+            <td><?php echo $nota["Fecha_Entrega_Estimada"]; ?></td>
+            <td><?php echo $nota["Hora_Entrega_Estimada"]; ?></td>
+            <td><a href="actualizar.php?Folio_Nota_PK=<?php echo $nota['Folio_Nota_PK']; ?>">Editar</a></td>
+          </tr>
+        <?php endforeach; ?>
+      <?php else: ?>
+        <tr>
+          <td colspan="7">No se encontraron resultados</td>
+        </tr>
+      <?php endif; ?>
+    </table>
   </section>
   <footer class="footer">
     <a href="../notas.php" class="btn_salir">Regresar</a>
