80fae323cf212e7e01c6f0e3f2d2ba0c5e2f6bf1
angelmz88 <joseangelmarquez080802@gmail.com>
2024-05-23 16:27:51 -0600

    Visualizacion de servicios externos

--- Pages/notas/servicio.php
+++ Pages/notas/servicio.php
@@ -1,7 +1,44 @@
 <?php
 include ("notas/../../../php/deep_sesion.php");
-?>
+include ("notas/../../../php/bd.php");
 
+// Función para obtener los datos de la base de datos
+function obtenerexternos($term = '')
+{
+  global $conn;
+  $sql = "SELECT * FROM tenido_reparacion";
+  if ($term != '') {
+    $sql .= " WHERE Folio_Nota_PK_FK LIKE '%$term%' OR Cantidad_Prendas_PK LIKE '%$term%' OR Tipo_Servicio LIKE '%$term%' 
+    OR Telefono_Colaborador_FK LIKE '%$term%' OR Estatus LIKE '%$term%'";
+  }
+  $result = $conn->query($sql);
+
+  $externos = array();
+
+  if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+      $externos[] = $row;
+    }
+  }
+  return $externos;
+}
+
+// Inicializar la variable $externos para evitar errores
+$externos = array();
+
+// Si es una solicitud AJAX, devolver los resultados en JSON
+if (isset($_GET['term'])) {
+  header('Content-Type: application/json'); // Asegura que el contenido sea JSON
+  $term = $_GET['term'];
+  $externos = obtenerexternos($term);
+  echo json_encode($externos);
+  $conn->close();
+  exit();
+}
+
+// Obtener todas las externos al cargar la página
+$externos = obtenerexternos();
+?>
 <!DOCTYPE html>
 <html lang="es">
 
@@ -12,7 +49,38 @@ include ("notas/../../../php/deep_sesion.php");
   <link rel="stylesheet" href="../../css/style.css" />
   <link rel="stylesheet" href="../../css/consulta.css" />
   <link rel="shortcut icon" href="../../img/global/lavanderia.png" />
-  <title>Notas</title>
+  <title>externos</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script>
+    $(document).ready(function () {
+      $('#search').on('keyup', function () {
+        var searchTerm = $(this).val();
+        $.ajax({
+          url: '',
+          type: 'GET',
+          data: { term: searchTerm },
+          dataType: 'json',
+          success: function (data) {
+            var tableContent = '<table><tr><th>Folio_externo_PK_FK</th><th>Cantidad_Prendas_PK</th><th>Tipo_Servicio</th><th>Telefono_Colaborador_FK</th><th>Estatus</th><th>Acciones</th></tr>';
+            if (data.length > 0) {
+              $.each(data, function (index, externo) {
+                tableContent += '<tr><td>' + externo.Folio_externo_PK + '</td><td>' + externo.Cantidad_Prendas_PK +
+                  '</td><td>' + externo.Tipo_Servicio + '</td><td>' + externo.Telefono_Colaborador_FK +
+                  '</td><td>' + externo.Estatus + '</td><td>Seguimiento</a></td></tr>';
+              });
+            } else {
+              tableContent += '<tr><td colspan="6">No se encontraron resultados</td></tr>';
+            }
+            tableContent += '</table>';
+            $('#resultados').html(tableContent);
+          },
+          error: function (jqXHR, textStatus, errorThrown) {
+            console.log(textStatus, errorThrown);
+          }
+        });
+      });
+    });
+  </script>
 </head>
 
 <body>
@@ -21,13 +89,39 @@ include ("notas/../../../php/deep_sesion.php");
   </header>
   <section class="hero">
     <div class="hero-cover">
-      <h1>SERVICIO EXTERNO</h1>
+      <h1>CONSULTAR externoS</h1>
     </div>
   </section>
   <section class="options">
     <div class="buscador">
-      <input type="text" />
+      <input type="text" id="search" placeholder="Buscar externo..." />
     </div>
+  </section>
+  <section id="resultados" class="resultados">
+    <table class="table">
+      <tr>
+        <th>Folio_Nota_PK_FK</th>
+        <th>Cantidad_Prendas_PK</th>
+        <th>Tipo_Servicio</th>
+        <th>Telefono_Colaborador_FK</th>
+        <th>Estatus</th>
+      </tr>
+      <?php if (count($externos) > 0): ?>
+        <?php foreach ($externos as $externo): ?>
+          <tr>
+            <td><?php echo $externo["Folio_Nota_PK_FK"]; ?></td>
+            <td><?php echo $externo["Cantidad_Prendas_PK"]; ?></td>
+            <td><?php echo $externo["Tipo_Servicio"]; ?></td>
+            <td><?php echo $externo["Telefono_Colaborador_FK"]; ?></td>
+            <td><?php echo $externo["Estatus"]; ?></td>
+          </tr>
+        <?php endforeach; ?>
+      <?php else: ?>
+        <tr>
+          <td colspan="7">No se encontraron resultados</td>
+        </tr>
+      <?php endif; ?>
+    </table>
   </section>
   <footer class="footer">
     <a href="../notas.php" class="btn_salir">Regresar</a>
